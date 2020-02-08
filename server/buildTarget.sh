0ba41e28da8a6cfc5e954623b3cd9fe05779f4b8
Michal Vala <michal.vala@gmail.com>
2020-02-08 07:32:39 +0100

    build and upload script

---
+++ server/buildTarget.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -x
+set -e
+
+TARGET_IP=192.168.1.101
+TARGET_OS=linux
+TARGET_ARCH=arm
+
+OUT=out/payterm
+TARGET_OUT=/home/pi/payterm
+
+GOOS=${TARGET_OS} GOARCH=${TARGET_ARCH} go build -o ${OUT} main.go
+
+scp -i "$(cygpath -pm ~/.ssh/id_rsa)" ${OUT} pi@${TARGET_IP}:${TARGET_OUT}
+# shellcheck disable=SC2029
+ssh -i "$(cygpath -pm ~/.ssh/id_rsa)" pi@${TARGET_IP} "chmod +x ${TARGET_OUT}"
