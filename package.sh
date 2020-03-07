955c3e3b890ed404d2737f4e9ee01208c416fa00
Michal Vala <michal.vala@gmail.com>
2020-03-07 05:49:03 +0100

    ok, forgot this

--- package.sh
+++ package.sh
@@ -5,28 +5,28 @@ set -e
 
 SWD=$( cd "$(dirname "${0}")" && pwd )
 OUT_DIR=${SWD}/out
-TERM_DIR=${SWD}/term
+CONTROLLER_DIR=${SWD}/controller
 PERIPHS_DIR=${SWD}/peripherals
 
 
 rm -rf "${OUT_DIR}"
 mkdir -p "${OUT_DIR}"
 
-pushd "${TERM_DIR}" > /dev/null
+pushd "${CONTROLLER_DIR}" > /dev/null
 printf "refreshing go modules ... "
 go mod download
 printf "done\n"
 
-printf "building terminal server x64 linux ... "
-GOOS=linux GOARCH=amd64 go build -o "${OUT_DIR}"/term_x64 cmd/term.go
+printf "building terminal controller x64 linux ... "
+GOOS=linux GOARCH=amd64 go build -o "${OUT_DIR}"/controller_x64 cmd/controller.go
 printf "done\n"
 
-printf "building terminal server arm linux ... "
-GOOS=linux GOARCH=arm go build -o "${OUT_DIR}"/term_arm cmd/term.go
+printf "building terminal controller arm linux ... "
+GOOS=linux GOARCH=arm go build -o "${OUT_DIR}"/controller_arm cmd/controller.go
 printf "done\n"
 
-printf "building terminal server x64 windows ... "
-GOOS=windows GOARCH=amd64 go build -o "${OUT_DIR}"/term_win.exe cmd/term.go
+printf "building terminal controller x64 windows ... "
+GOOS=windows GOARCH=amd64 go build -o "${OUT_DIR}"/controller_win.exe cmd/controller.go
 printf "done\n"
 popd > /dev/null
 
