3efdf75006fb4b3c359bf5d41701af8657bdc4fb
Michal Vala <mvala@redhat.com>
2020-04-09 21:09:32 +0200

    launcher should run controller and mocked peripherals, but looks like does not work much yet

--- launcher/cmd/run.go
+++ launcher/cmd/run.go
@@ -8,26 +8,57 @@ import (
 	"time"
 )
 
+const PLATFORM_X64 = "x64"
+const PLATFORM_WIN = "WIN"
+const PLATFORM_ARM = "arm"
+
 type conf struct {
 	paytermPath string
+	platform string
+
+	// private
+	launcher string
 }
 
 func main() {
 	conf := parseArgs()
 
-	log.Println(conf)
+	//for _, e := range os.Environ() {
+	//	log.Printf("%s", e)
+	//}
 
-	cmd := exec.Command(conf.paytermPath + "/controller_win.exe")
+	switch conf.platform {
+	case PLATFORM_ARM:
+		conf.launcher = "controller_arm"
+	case PLATFORM_WIN:
+		conf.launcher = "controller_win.exe"
+	case PLATFORM_X64:
+		conf.launcher = "controller_x64"
+	default:
+		log.Fatalf("invalid platform [%s]", conf.platform)
+	}
+	go execProcess("/bin/python3", conf.paytermPath + "/mock/buzzer.py")
+	go execProcess("/bin/python3", conf.paytermPath + "/mock/display.py")
+	go execProcess(conf.paytermPath + "/" + conf.launcher)
 
+	time.Sleep(10 * time.Second)
+	log.Println("ende ...")
+}
+
+func execProcess(command string, args ...string) {
+	log.Printf("running [%s %s]", command, args)
+	cmd := exec.Command(command, args...)
 	cmd.Stdout = os.Stdout
 
 	if err := cmd.Start(); err != nil {
+		log.Println("blabol", command)
 		log.Fatal(err)
-	} else {
-		time.Sleep(5 * time.Second)
-		if killErr := cmd.Process.Kill(); killErr != nil {
-			log.Fatal(killErr)
+	}else {
+		log.Printf("about to wait for [%s] [%+v] ... \n", command, cmd.ProcessState)
+		if err := cmd.Wait(); err != nil {
+			log.Printf("process [%s] error with [%s] [%+v] \n", command, err, cmd.ProcessState)
 		}
+		log.Printf("waitin done for [%s] [%+v] ... \n", command, cmd.ProcessState)
 	}
 }
 
@@ -35,6 +66,7 @@ func parseArgs() *conf {
 	var conf = &conf{}
 
 	flag.StringVar(&conf.paytermPath, "paytermPath", "", "Path to dir where are controller and all python peripherals.")
+	flag.StringVar(&conf.platform, "platform", "", "Platform [x64, arm, win].")
 
 	flag.Parse()
 
@@ -45,6 +77,16 @@ func parseArgs() *conf {
 		} else {
 			flag.PrintDefaults()
 			log.Fatal("No payterm path defined. User either `-paytermPath` param or PAYTERM_PATH env variable")
+		}
+	}
+
+	if conf.platform == "" {
+		if platform, ok := os.LookupEnv("PAYTERM_PLATFORM"); ok {
+			conf.platform = platform
+			log.Printf("Using env variable PAYTERM_PLATFORM [%s]", conf.platform)
+		} else {
+			flag.PrintDefaults()
+			log.Fatal("No platform defined. User either `-platform` param or PAYTERM_PLATFORM env variable")
 		}
 	}
 
